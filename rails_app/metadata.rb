d7f908c24008471469f75902fdefb14f679cac12
Ubuntu <ubuntu@ip-172-31-29-218.ec2.internal>
2017-08-18 20:40:23 +0000

    Initial commit

---
+++ rails_app/metadata.rb
@@ -0,0 +1,21 @@
+name 'rails_app'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures rails_app'
+long_description 'Installs/Configures rails_app'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+depends 'apt'
+depends 'nodejs'
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/rails_app/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/rails_app'
---
+++ rails_app/recipes/yarn.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook:: rails_app
+# Recipe:: yarn
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+execute 'install node source' do
+    command 'curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - '
+end
+
+execute 'install node' do
+    commadn 'sudo apt-get install -y nodejs'
+end
+
+execute 'add key' do 
+    command 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - '
+end
+execute 'add source' do
+    command 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
+end
+execute 'update apt' do
+    command 'sudo apt-get update'
+end
+
+apt_package 'yarn'
